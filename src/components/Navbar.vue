ef44c746ed6361e5757f9784828162466c72ba3f
Quezty <storlijoachim@gmail.com>
2024-04-05 13:20:17 +0200

    started on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,19 @@
 <template>
-
+    <nav class="w-3/4 mx-auto mt-6 flex">
+        <img src="../assets/images/Breilan-White-on-Black-Logo.png" alt="" class="w-1/12">
+        <div class="flex flex-col items-start justify-center ml-4 ">    
+            <h1 class="font-extrabold text-3xl">
+                BreiLAN
+            </h1>
+            <p class="font-medium">
+                Et arrangement av ungdom, for ungdom
+            </p>
+        </div>
+    </nav>
 </template>
 
 <script>
-
+    
 </script>
 
 <style scoped>
