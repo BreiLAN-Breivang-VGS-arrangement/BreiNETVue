ca99289a73ab4c13ad237d3ff587243ea7ba39ce
Quezty <storlijoachim@gmail.com>
2024-04-05 19:28:18 +0200

    added routes and components for each route

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './components/Home.vue';
+import Crewpanel from './components/Crewpanel.vue';
+import Login from './components/Login.vue';
+import Tournaments from './components/Tournaments.vue';
+import Register from './components/Register.vue';
 
 const routes = [
-    
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/Register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/Login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/Tournaments',
+        name: 'Tournaments',
+        component: Tournaments
+    },
+    {
+        path: '/Crewpanel',
+        name: 'Crewpanel',
+        component: Crewpanel
+    },
 ];
 
 const router = createRouter({
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,33 @@
 <template>
-    <nav class="w-3/4 mx-auto mt-6 flex">
-        <img src="../assets/images/Breilan-White-on-Black-Logo.png" alt="" class="w-1/12">
-        <div class="flex flex-col items-start justify-center ml-4 ">    
-            <h1 class="font-extrabold text-3xl">
-                BreiLAN
-            </h1>
-            <p class="font-medium">
-                Et arrangement av ungdom, for ungdom
-            </p>
+    <nav class="w-2/4 mx-auto mt-6 flex flex-row">
+        <div class="w-3/4">        
+            <img src="../assets/images/Breilan-White-on-Black-Logo.png" alt="" class="w-1/4">
+            <div class="flex flex-col items-start justify-center ml-4 w-3/4">    
+                <h1 class="font-extrabold text-3xl">
+                    BreiLAN
+                </h1>
+                <p class="font-medium ">
+                    Et arrangement av ungdom, for ungdom
+                </p>
+            </div>
+        </div>
+
+        <div class="flex flex-row justify-center w-1/4">
+            <RouterLink to="/" href="#" class="" >
+                Home
+            </RouterLink>
+            <RouterLink to="/Register" href="#" class="" >
+                Register
+            </RouterLink>
+            <RouterLink to="/Login" href="#" class="" >
+                Login
+            </RouterLink>
+            <RouterLink to="/Tournaments" href="#" class="">
+                Tournaments
+            </RouterLink>
+            <RouterLink to="/Crewpanel" href="#" class="">
+                Crew panel
+            </RouterLink>
         </div>
     </nav>
 </template>
