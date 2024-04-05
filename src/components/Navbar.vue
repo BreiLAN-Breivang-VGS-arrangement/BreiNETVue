a592548873c5874e564bc408370a829944c73a1f
Quezty <storlijoachim@gmail.com>
2024-04-05 20:44:05 +0200

    made structure for navbar, bare bones styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="w-2/4 mx-auto mt-6 flex flex-row">
-        <div class="w-3/4">        
-            <img src="../assets/images/Breilan-White-on-Black-Logo.png" alt="" class="w-1/4">
-            <div class="flex flex-col items-start justify-center ml-4 w-3/4">    
+        <div class="w-1/2 flex flex-row">        
+            <img src="../assets/images/Breilan-White-on-Black-Logo.png" alt="" class="w-1/4 rounded">
+            <div class="flex flex-col items-start justify-center ml-4 w-4/6">    
                 <h1 class="font-extrabold text-3xl">
                     BreiLAN
                 </h1>
@@ -12,7 +12,7 @@
             </div>
         </div>
 
-        <div class="flex flex-row justify-center w-1/4">
+        <div class="flex flex-row justify-between items-center w-1/2">
             <RouterLink to="/" href="#" class="" >
                 Home
             </RouterLink>
