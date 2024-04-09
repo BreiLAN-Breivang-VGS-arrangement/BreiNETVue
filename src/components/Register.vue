491a732c5dec56c0768f73c114389bda60e00c9f
Quezty <storlijoachim@gmail.com>
2024-04-09 09:26:03 +0200

    added first version of form sending to register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,18 +1,18 @@
 <template>
 
-    <form @submit.prevent="sendTournament()" >
+    <form @submit.prevent="registerAccount()" >
         <label for="username">
-
+            Brukernavn
         </label>
         <input type="text" v-model="username">
         
         <label for="password">
-
+            Passord
         </label>
         <input type="text" v-model="password">
         
         <label for="secret_key">
-
+            Hemmelig nøkkel
         </label>
         <input type="text" v-model="secret_key">
     </form>
@@ -32,21 +32,21 @@ export default {
     },
 
     methods: {
-        async sendTournament(){
+        async registerAccount(){
             if(this.first_name == '' || this.last_name == '' || this.email == ''){
                 try{ 
-                    await axios.post('http://127.0.0.1:5000/create_contact', {
+                    await axios.post('http://127.0.0.1:5000/register', {
                         username: this.username,
                         password: this.password,
                         secret_key: this.secret_key,
                     });
                     location.reload();
                 } catch(error) {
-                    alert('Something went wrong in sending the contact')
+                    alert('Noe gikk galt under opprettingen av brukeren, brukte du rett hemmelig nøkkel?')
                     console.log(error)
                 };
             } else {
-                alert("please write firstname, lastname, and email!")
+                alert("Du er nødt til å fylle alle tre feltene for å kunne opprette bruker!")
             }
         }
     }
