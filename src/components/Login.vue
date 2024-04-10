bc4af6311d729d369391db548c8633ae38e8a95c
Quezty <storlijoachim@gmail.com>
2024-04-10 20:31:01 +0200

    added encoding and header to axios request

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -16,6 +16,9 @@
         </button>
     </form>
 
+    <button type="button" @click="currentUser()">currentUser</button>
+    <p > {{ user }} </p>
+
 </template>
 
 <script>
@@ -26,6 +29,7 @@ export default {
         return {
             username: '',
             password: '',
+            user: ''
         };
     },
 
@@ -33,17 +37,29 @@ export default {
         async login(){
             if(this.username != '' && this.password != ''){
                 try{ 
-                    await axios.post('http://127.0.0.1:5000/login', {
-                        username: this.username,
-                        password: this.password,
-                    });
+                    await axios.post('http://127.0.0.1:5000/login', 
+                        // Form data
+                        `username=${encodeURIComponent(this.username)}&password=${encodeURIComponent(this.password)}`, 
+                        // Headers
+                        {
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'                            }
+                        });
                 } catch(error) {
                     alert('Vi kunne ikke logge deg inn, sikker på at det er rett brukernavn og passord?')
                     console.log(error)
                 };
             } else {
-                alert("Husk både passord OG brukernavn!")
+                alert("Husk både passord og brukernavn!")
             }
+        },
+
+        currentUser() {
+            axios.get('http://127.0.0.1:5000/currentuser')
+            .then( function(res) {
+                console.log(res.data)
+                this.currentUser = res.data
+            })
         }
     }
 };
